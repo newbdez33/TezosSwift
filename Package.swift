843bcb3fd1157210e16d6caccbbb504930a096f7
Jacky <newbdez33@gmail.com>
2020-10-07 17:29:58 +0900

    update depencencies SPM

--- Package.swift
+++ Package.swift
@@ -12,6 +12,24 @@ let package = Package(
             name: "TezosSwift",
             targets: ["TezosSwift"]),
     ],
+    dependencies: [
+        .package(
+            url: "https://github.com/krzyzanowskim/CryptoSwift", 
+            from: "1.3.2"
+        ),
+        .package(
+            url: "https://github.com/attaswift/BigInt", 
+            from: "5.2.0"
+        ),
+        .package(
+            url: "https://github.com/newbdez33/MnemonicKit", 
+            from: "1.3.21"
+        ),
+        .package(
+            url: "https://github.com/jedisct1/swift-sodium", 
+            from: "0.8.0"
+        ),
+    ],
     targets: [
         .target(
             name: "TezosSwift",
