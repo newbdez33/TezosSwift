c4c39afe100002f010351cd9b2499483ceadc89c
Jacky <newbdez33@gmail.com>
2020-10-08 11:37:15 +0900

    update SPM

--- Package.swift
+++ Package.swift
@@ -1,22 +1,17 @@
 // swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "TezosSwift",
-    platforms: [
-        .macOS(.v10_12),
-        .iOS(.v9)//,
-    ],
     products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "TezosSwift",
             targets: ["TezosSwift"]),
     ],
     dependencies: [
-        .package(
-            url: "https://github.com/krzyzanowskim/CryptoSwift", 
-            from: "1.3.2"
-        ),
         .package(
             url: "https://github.com/attaswift/BigInt", 
             from: "5.2.0"
@@ -27,14 +22,17 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/jedisct1/swift-sodium", 
-            .branch("master")
+            from: "0.9.0"
         ),
     ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TezosSwift",
-            dependencies: ["CryptoSwift", "BigInt", "MnemonicKit", "Sodium"],
-            path: "TezosSwift",
-            exclude: ["Info.plist"]),
+            dependencies: ["BigInt", "MnemonicKit", "Sodium"]),
+        .testTarget(
+            name: "TezosSwiftTests",
+            dependencies: ["TezosSwift"]),
     ]
 )
